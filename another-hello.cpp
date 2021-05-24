63e86fdb6206d8365963207faac8a39eacfbf97e
shyrv <yuxuanshyr@gmail.com>
2021-05-24 21:05:22 +0800

    rollback testing

--- another-hello.cpp
+++ another-hello.cpp
@@ -5,5 +5,6 @@ using namespace std;
 int main(){
 	cout << "another world, another hello!" << endl;
 	cout << "branching is hard" << endl;
+	cout << "learning how to rollback" << endl;
 	return 0;
 }
