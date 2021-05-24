775cc76924a0a2ade7b6314688df664582e4b443
shyrv <yuxuanshyr@gmail.com>
2021-05-24 21:07:27 +0800

    Revert "rollback testing"
    This reverts commit 63e86fdb6206d8365963207faac8a39eacfbf97e.

--- another-hello.cpp
+++ another-hello.cpp
@@ -5,6 +5,5 @@ using namespace std;
 int main(){
 	cout << "another world, another hello!" << endl;
 	cout << "branching is hard" << endl;
-	cout << "learning how to rollback" << endl;
 	return 0;
 }
